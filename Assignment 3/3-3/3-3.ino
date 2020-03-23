918afea81d7d1223b542c93ebdcfb04fa93dc596
Windwill <jojoeza10@gmail.com>
2020-03-23 16:59:10 +0700

    HW 3-3 Code

--- Assignment 3/3-3/3-3.ino
+++ Assignment 3/3-3/3-3.ino
@@ -1,7 +1,23 @@
 void setup(){
+    pinMode(2,OUTPUT);
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(9,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(11,OUTPUT);
+    pinMode(12,OUTPUT);
+    pinMode(13,OUTPUT);
 
 }
 
 void loop(){
-    
+    int x=0;
+    for(x=0;x<=13;x++){
+        digitalWrite(x,1);
+        delay(250);
+    }
 }
