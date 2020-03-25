d8a4e723ffc8f43be822a44a1cefe90f46a6103c
intira <bluewhalechanal@gmail.com>
2020-03-25 13:11:01 +0700

    3-1

--- Assignment 3/3-1/3-1.ino
+++ Assignment 3/3-1/3-1.ino
@@ -23,14 +23,6 @@ void loop()
     digitalWrite(10,1);
     digitalWrite(9,1);
     digitalWrite(8,1);
-    delay(500);
-    digitalWrite(13,0);
-    digitalWrite(12,0);
-    digitalWrite(11,0);
-    digitalWrite(10,0);
-    digitalWrite(9,0);
-    digitalWrite(8,0);
-    delay(500);
     digitalWrite(7,1);
     digitalWrite(6,1);
     digitalWrite(5,1);
@@ -38,6 +30,12 @@ void loop()
     digitalWrite(3,1);
     digitalWrite(2,1);
     delay(500);
+    digitalWrite(13,0);
+    digitalWrite(12,0);
+    digitalWrite(11,0);
+    digitalWrite(10,0);
+    digitalWrite(9,0);
+    digitalWrite(8,0);
     digitalWrite(7,0);
     digitalWrite(6,0);
     digitalWrite(5,0);
