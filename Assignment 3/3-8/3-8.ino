68c22f5f99d9d6f20d8e762a6dc47c81a64cdaad
Windwill <jojoeza10@gmail.com>
2020-03-26 19:38:46 +0700

    update 3-8 code

--- Assignment 3/3-8/3-8.ino
+++ Assignment 3/3-8/3-8.ino
@@ -1,7 +1,7 @@
 void setup(){
     Serial.begin(9600);
     int i = 1;
-    int id = 5+9+0+8+0+5+0+1+6+1+7; //42
+    int id = 6+2+1+2+0+5+0+1+0+0+9; //26
     for(i=1;i<=1000;i++){
         if(i % id ==0){
             Serial.println(i);
