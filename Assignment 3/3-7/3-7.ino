e47cd3fdab8f0d440aef6f12d913383525edfa59
Windwill <jojoeza10@gmail.com>
2020-03-26 19:12:03 +0700

    Update code 3-7

--- Assignment 3/3-7/3-7.ino
+++ Assignment 3/3-7/3-7.ino
@@ -45,7 +45,21 @@ void loop(){
             delay(time*expo);
         }
         
+        
     }
+    digitalWrite(2,0);
+    digitalWrite(3,0);
+    digitalWrite(4,0);
+    digitalWrite(5,0);
+    digitalWrite(6,0);
+    digitalWrite(7,0);
+    digitalWrite(8,0);
+    digitalWrite(9,0);
+    digitalWrite(10,0);
+    digitalWrite(11,0);
+    digitalWrite(12,0);
+    digitalWrite(13,0);
+    delay(2000);
         
 
     
