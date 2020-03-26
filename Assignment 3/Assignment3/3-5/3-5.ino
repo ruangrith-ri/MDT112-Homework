3b888882b93405a65dffbeec6f2202da5a386053
thanyakorn.nak <thanyakorn.nak@mail.kmutt.ac.th>
2020-03-26 20:26:24 +0700

    new commit

--- Assignment 3/Assignment3/3-5/3-5.ino
+++ Assignment 3/Assignment3/3-5/3-5.ino
@@ -1,18 +1,23 @@
-void setup() {	  
-   Serial.begin(9600);
-    pinMode(2, INPUT_PULLUP); 
-    pinMode(3, OUTPUT); 
-    pinMode(4, OUTPUT); 
-    pinMode(5, OUTPUT); 
-    pinMode(6, OUTPUT); 
-    pinMode(7, OUTPUT); 
-    pinMode(8, OUTPUT); 
-    pinMode(9, OUTPUT); 
-    pinMode(10, OUTPUT); 
-    pinMode(11, OUTPUT); 
-    pinMode(12, OUTPUT); 
-    pinMode(13, OUTPUT);
-    }
+void setup() {
+pinMode(13,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(2,OUTPUT);
+
+
+}
+   
+
 void loop() {
+
+
 
 }
