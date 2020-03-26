7da64b9e0393d2d9fb33bf547530aab0437ef108
Windwill <jojoeza10@gmail.com>
2020-03-26 19:32:26 +0700

    Upload 3-8 Code

--- Assignment 3/3-8/3-8.ino
+++ Assignment 3/3-8/3-8.ino
@@ -1,13 +1,14 @@
 void setup(){
     Serial.begin(9600);
-}
-
-void loop(){
     int i = 1;
-    int id = 6+2+1+2+0+5+0+1+0+0+9; //26
+    int id = 5+9+0+8+0+5+0+1+6+1+7; //42
     for(i=1;i<=1000;i++){
         if(i % id ==0){
             Serial.println(i);
         }
     }
+}
+
+void loop(){
+    
 }
