311d604a119b4ff7b300756b785eb0b895b5a2a7
katapat <61570104+katapat@users.noreply.github.com>
2020-03-27 00:05:12 +0700

    Add files via upload

---
+++ Assignment 3/3-1/3-8.ino
@@ -0,0 +1,13 @@
+int i = 1;
+int id = 6+2+1+2+0+5+0+1+0+0+1;
+void setup(){
+    Serial.begin(9600);
+    {
+      for(i=1;i<=1000;i++){
+        if(i % id ==0){
+        Serial.println(i);
+      }
+    }
+}
+
+void loop(){
