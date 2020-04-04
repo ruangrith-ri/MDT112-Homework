f5dc0d29423a26a7c5850d8a64fef8ac90e8ddd1
Windwill <jojoeza10@gmail.com>
2020-04-04 18:52:45 +0700

    update fea 4-4 code

--- Assignment 4/4-4/feature4-4.ino
+++ Assignment 4/4-4/feature4-4.ino
@@ -1,5 +1,11 @@
 String examPassChack(int yourScore, int fullScore) {
   String result;
+  if (yourScore> (fullScore/2)){
+      result = "Your are " + String(yourScore) + " form " + String(fullScore) + "point in last exam.\nPass exam Congratulations.";
+  }
+  if (yourScore<= (fullScore/2)){
+      result = "Your are " + String(yourScore) + " form " + String(fullScore) + "point in last exam.\nFail exam Attempt again.";
+  }
 
 
   return result;
