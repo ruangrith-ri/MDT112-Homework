1dd79092aaefca8aea6996c48b27b3fb0459d42e
Peerapon Chaipat <kisi550d@gmail.com>
2020-04-05 19:45:03 +0700

    4-8 Clear

--- Assignment 4/4-8/feature4-8.ino
+++ Assignment 4/4-8/feature4-8.ino
@@ -1,5 +1,22 @@
 String multiplicationTable(int multiple) {
   String result;
-
+int i =multiple*1;int ii =multiple*2;int iii =multiple*3;int iv =multiple*4;int v =multiple*5;int vi =multiple*6;
+int vii =multiple*7;int viii =multiple*8;int ix =multiple*9;int x =multiple*10;int xi =multiple*11;int xii =multiple*12;
+Serial.println("-------------------------------------------------");
+Serial.println("|                       "+String(multiple)+"                 \t|");
+Serial.println("|-----------------------------------------------|");
+Serial.println("|\t"+String(multiple)+"\t*\t1\t=\t"+String(i)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t2\t=\t"+String(ii)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t3\t=\t"+String(iii)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t4\t=\t"+String(iv)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t5\t=\t"+String(v)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t6\t=\t"+String(vi)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t7\t=\t"+String(vii)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t8\t=\t"+String(viii)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t9\t=\t"+String(ix)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t10\t=\t"+String(x)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t11\t=\t"+String(xi)+"\t|");
+Serial.println("|\t"+String(multiple)+"\t*\t12\t=\t"+String(xii)+"\t|");
+Serial.println("-------------------------------------------------");
   return result;
 }
