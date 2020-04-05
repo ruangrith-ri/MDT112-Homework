4fda00129a9afd6e55acba17e4742bae738dcdd1
Peerapon Chaipat <kisi550d@gmail.com>
2020-04-05 17:56:52 +0700

    4-4 Clear

--- Assignment 4/4-4/feature4-4.ino
+++ Assignment 4/4-4/feature4-4.ino
@@ -1,6 +1,13 @@
 String examPassChack(int yourScore, int fullScore) {
   String result;
-
+    if((yourScore)>=(fullScore/2)){
+        Serial.println("Your are "+String(yourScore)+" point from "+String(fullScore)+" point in last exam.");
+        Serial.println("Pass exam Congratulations.");
+    }
+    else if((yourScore)<(fullScore/2)){
+        Serial.println("Your are "+String(yourScore)+" point from "+String(fullScore)+" point in last exam.");
+        Serial.println("Fail exam Attempt again.");
+    }
 
   return result;
 }
