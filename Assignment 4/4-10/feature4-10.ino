c3ea2093b7b62457c3c06eef6272c908f800858f
Noppamas045 <noppamas.sang@mail.kmutt.ac.th>
2020-04-06 04:32:44 +0700

    edit2  ;;;<;;;;

--- Assignment 4/4-10/feature4-10.ino
+++ Assignment 4/4-10/feature4-10.ino
@@ -1,18 +1,24 @@
 String primeNumber(int maxNumber) {
   String result;
-  
-  for (int i = 1; i <= maxNumber; i++) {
-    if ((i == 1 || i % 2 == 0 || i % 3 == 0 || i % 7 == 0) &&
-        (i != 2 && i != 3 && i != 5 && i != 7)) {
-
-      continue;
-    }
-
-    result = result +  String(i);
-    result = result + "\t";
-                   
+  for (int i = 2; i <= maxNumber; i++) {
+      bool prim = true;
+      for (int j =2 ;j*j <= i ; j++) {
+          if(i % j == 0){
+              prim = false;
+              break;
+          }
+      }
+      if(prim)
+      
+      result = result + "\t" +String(i); 
+      
 
   }
+
+  
+  
+
+  
 
   return result;
 }
