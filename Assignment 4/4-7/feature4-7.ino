08189a3efa1a6e388b218bef27b67f28fbec9277
Windwill <jojoeza10@gmail.com>
2020-04-05 13:24:30 +0700

    update fea4-7 code

--- Assignment 4/4-7/feature4-7.ino
+++ Assignment 4/4-7/feature4-7.ino
@@ -3,7 +3,13 @@ String multiplication(int multiple){
     int calculation;
     for(int i=1;i<=12;i++){
         calculation = i*multiple;
-        combine = String(multiple) + " * " +String(i)+ " = " + String(calculation) + "\n";
+        if (i>=10){
+            combine = String(multiple) + "\r * \r" +String(i)+ "\r =  \r" + String(calculation) + "\n";
+        }
+        else{
+            combine = String(multiple) + "\r * \r" +String(i)+ "\r  =  \r" + String(calculation) + "\n";
+        }
+        
         combine2 = combine2+combine;
         
     }
