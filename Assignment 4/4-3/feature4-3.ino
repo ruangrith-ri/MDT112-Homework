4d4623568b7b469896c75bd502cef197222f719a
intira-bk <61378901+intira-bk@users.noreply.github.com>
2020-04-05 20:56:00 +0700

    Update feature4-3.ino

--- Assignment 4/4-3/feature4-3.ino
+++ Assignment 4/4-3/feature4-3.ino
@@ -1,5 +1,287 @@
 String numberToText(int age) {
-  String result;
+String result;
+  if(age == 1)
+  {
+      result = "one";
+  }
+  if(age == 2)
+  {
+      result = "two";
+  }
+  if(age == 3)
+  {
+      result = "three";
+  }
+  if(age == 4)
+  {
+      result = "four";
+  }
+  if(age == 5)
+  {
+      result = "five";
+  }
+  if(age == 6)
+  {
+      result = "six";
+  }
+  if(age == 7)
+  {
+      result = "seven";
+  }
+  if(age == 8)
+  {
+      result = "eight";
+  }
+  if(age == 9)
+  {
+      result = "nine";
+  }
+  if(age == 10)
+  {
+      result = "ten";
+  }
+  if(age == 11)
+  {
+      result = "eleven";
+  }
+  if(age == 12)
+  {
+      result = "twelve";
+  }
+  if(age == 13)
+  {
+      result = "thirteen";
+  }
+  if(age == 14)
+  {
+      result = "fourteen";
+  }
+  if(age == 15)
+  {
+      result = "fifteen";
+  }
+  if(age == 16)
+  {
+      result = "sixteen";
+  }
+  if(age == 17)
+  {
+      result = "seventeen";
+  }
+  if(age == 18)
+  {
+      result = "eighteen";
+  }
+  if(age == 19)
+  {
+      result = "nineteen";
+  }
+  if(age == 20)
+  {
+      result = "twenty";
+  }
+  if(age == 21)
+  {
+      result = "twenty-one";
+  }
+  if(age == 22)
+  {
+      result = "twenty-two";
+  }
+  if(age == 23)
+  {
+      result = "twenty-three";
+  }
+  if(age == 24)
+  {
+      result = "twenty-four";
+  }
+  if(age == 25)
+  {
+      result = "twenty-five";
+  }
+  if(age == 26)
+  {
+      result = "twenty-six";
+  }
+  if(age == 27)
+  {
+      result = "twenty-seven";
+  }
+  if(age == 28)
+  {
+      result = "twenty-eight";
+  }
+  if(age == 29)
+  {
+      result = "twenty-nine";
+  }
+  if(age == 30)
+  {
+      result = "thirty";
+  }
+  if(age == 31)
+  {
+      result = "thirty-one";
+  }
+  if(age == 32)
+  {
+      result = "thirty-two";
+  }
+  if(age == 33)
+  {
+      result = "thirty-three";
+  }
+  if(age == 34)
+  {
+      result = "thirty-four";
+  }
+  if(age == 35){
+      result = "thirty-five";
+  }
+  if(age == 36)
+  {
+      result = "thirty-six";
+  }
+  if(age == 37)
+  {
+      result = "thirty-seven";
+  }
+  if(age == 38)
+  {
+      result = "thirty-eight";
+  }
+  if(age == 39)
+  {
+      result = "thirty-nine";
+  }
+
+  if(age == 40)
+  {
+      result = "fourty";
+  }
+  if(age == 41)
+  {
+      result = "fourty-one";
+  }
+  if(age == 42)
+  {
+      result = "fourty-two";
+  }
+  if(age == 43)
+  {
+      result = "fourty-three";
+  }
+  if(age == 44)
+  {
+      result = "fourty-four";
+  }
+  if(age == 45)
+  {
+      result = "fourty-five";
+  }
+  if(age == 46)
+  {
+      result = "fourty-six";
+  }
+  if(age == 47)
+  {
+      result = "fourty-seven";
+  }
+  if(age == 48)
+  {
+      result = "fourty-eight";
+  }
+  if(age == 49)
+  {
+      result = "fourty-nine";
+  }
+
+  if(age == 50)
+  {
+      result = "fifty";
+  }
+  if(age == 51)
+  {
+      result = "fifty-one";
+  }
+  if(age == 52)
+  {
+      result = "fifty-two";
+  }
+  if(age == 53)
+  {
+      result = "fifty-three";
+  }
+  if(age == 54)
+  {
+      result = "fifty-four";
+  }
+  if(age == 55)
+  {
+      result = "fifty-five";
+  }
+  if(age == 56)
+  {
+      result = "fifty-six";
+  }
+  if(age == 57)
+  {
+      result = "fifty-seven";
+  }
+  if(age == 58)
+  {
+      result = "fifty-eight";
+  }
+  if(age == 59)
+  {
+      result = "fifty-nine";
+  }
+
+  if(age == 60)
+  {
+      result = "sixty";
+  }
+  if(age == 61)
+  {
+      result = "sixty-one";
+  }
+  if(age == 62)
+  {
+      result = "sixty-two";
+  }
+  if(age == 63)
+  {
+      result = "sixty-three";
+  }
+  if(age == 64)
+  {
+      result = "sixty-four";
+  }
+  if(age == 65)
+  {
+      result = "sixty-five";
+  }
+  if(age == 66)
+  {
+      result = "sixty-six";
+  }
+  if(age == 67)
+  {
+      result = "sixty-seven";
+  }
+  if(age == 68)
+  {
+      result = "sixty-eight";
+  }
+  if(age == 69)
+  {
+      result = "sixty-nine";
+  }
+  if(age == 70)
+  {
+      result = "seventy";
+  }
 
   return result;
 }
