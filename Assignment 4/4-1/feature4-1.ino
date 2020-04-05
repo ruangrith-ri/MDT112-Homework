5a23480114aeb28e4fe0a5321ffed8bfca378321
sawarin <sawarin.katoy@mail.kmutt.ac.th>
2020-04-05 22:11:39 +0700

    Homework1-2,4-7

--- Assignment 4/4-1/feature4-1.ino
+++ Assignment 4/4-1/feature4-1.ino
@@ -1,7 +1,7 @@
 String yourAreYoung(int age) {
-  String result;
-
-
-
+    String result = "i'm" + String(age) + "year old";
+    if (age > 0 && age <= 18) {
+    String result = "i'm" + String(age) + "year old\n" "Your are young";
+    }
   return result;
 }
--- Assignment 4/4-7/feature4-7.ino
+++ Assignment 4/4-7/feature4-7.ino
@@ -1,6 +1,7 @@
 String multiplication(int multiple){
     String result;
-
-
+    for(int i = 1 ; i<=12 ; i++){
+        result = String(multiple) + "\t" "*" "\t" + "1" "\t" "=" "\t" + (multiple * i) + "\n";
+    }
     return result;
 }
