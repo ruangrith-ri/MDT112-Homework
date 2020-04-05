28443389f844d260bd2d776d76888a989fb67da2
intira-bk <61378901+intira-bk@users.noreply.github.com>
2020-04-05 21:10:27 +0700

    Update feature4-4.ino

--- Assignment 4/4-4/feature4-4.ino
+++ Assignment 4/4-4/feature4-4.ino
@@ -1,5 +1,13 @@
 String examPassChack(int yourScore, int fullScore) {
   String result;
+    if (yourScore>(fullScore/2)) 
+  {
+      result="you are"+ String(yourScore)+"form"+String(fullScore)+"point in last exam\n Pass exam Congratulations";
+  }
+  if(yourScore<=(fullScore/2))
+  {
+      result="you are"+String(yourScore)+"from"+String(fullScore)+"point inlast exam\n  Fail exam Attempt agin";
+  }
 
 
   return result;
