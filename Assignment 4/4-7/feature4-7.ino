fbd61b1f6b4d2502d9ef3fab33e8c2f137894049
Windwill <jojoeza10@gmail.com>
2020-04-05 13:09:19 +0700

    update fea 4-7 code

--- Assignment 4/4-7/feature4-7.ino
+++ Assignment 4/4-7/feature4-7.ino
@@ -1,5 +1,14 @@
 String multiplication(int multiple){
-    String result;
+    String result,combine,combine2;
+    int calculation;
+    for(int i=1;i<=12;i++){
+        calculation = i*multiple;
+        combine = String(multiple) + " * " +String(i)+ " = " + String(calculation) + "\n";
+        combine2 = combine2+combine;
+        
+    }
+    result = combine2;
+    
 
 
     return result;
