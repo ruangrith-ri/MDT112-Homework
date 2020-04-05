366d5368e96402cc6adcb7a36494babe67c24a76
intira-bk <61378901+intira-bk@users.noreply.github.com>
2020-04-05 23:37:28 +0700

    Update feature4-5.ino

--- Assignment 4/4-5/feature4-5.ino
+++ Assignment 4/4-5/feature4-5.ino
@@ -1,5 +1,36 @@
 String gradeChack(int yourScore) {
   String result;
-
+  if (yourScore>=0&&yourScore<=54) 
+  {
+      result = "Your are"+String(yourScore)+"from 100 point in computer programming subject.\n You get grade F.See you again next semester ";
+  }
+  if (yourScore>=55&&yourScore<=59) 
+  {
+      result = "Your are"+String(yourScore)+"from 100 point in computer programing subject.\n You get grade D.";
+  }
+   if (yourScore>=60&&yourScore<=64) 
+  {
+      result = "Your are"+String(yourScore)+"from 100 point in computer programing subject.\n You get grade D+.";
+  }
+  if (yourScore>=65&&yourScore<=69) 
+  {
+      result = "Your are"+String(yourScore)+"from 100 point in computer programing subject.\n You get grade C.";
+  }
+  if (yourScore>=70&&yourScore<=74) 
+  {
+      result = "Your are"+String(yourScore)+"from 100 point in computer programing subject.\n You get grade C+.";
+  }
+  if (yourScore>=75&&yourScore<=79) 
+  {
+      result = "Your are"+String(yourScore)+"from 100 point in computer programing subject.\n You get grade B.";
+  }
+  if (yourScore>=80&&yourScore<=84) 
+  {
+      result = "Your are"+String(yourScore)+"from 100 point in computer programing subject.\n You get grade B+.";
+  }
+  if (yourScore>=85&&yourScore<=100) 
+  {
+      result = "Your are"+String(yourScore)+"from 100 point in computer programing subject.\n You get grade A.";
+  }
   return result;
 }
