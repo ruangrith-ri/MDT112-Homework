b61cf2c7c691ee60ea728829e368385cdfad2eec
Peerapon Chaipat <kisi550d@gmail.com>
2020-04-05 16:35:55 +0700

    4-1 Clear

--- Assignment 4/4-1/feature4-1.ino
+++ Assignment 4/4-1/feature4-1.ino
@@ -1,7 +1,12 @@
-String yourAreYoung(int age) {
+String yourAreYoung(int age) 
+ {
   String result;
-
-
+    
+  Serial.println(" I'm "+String(age)+" year old.");
+  if (age <18) 
+  {
+      Serial.println("Your are young.");
+  }
 
   return result;
 }
