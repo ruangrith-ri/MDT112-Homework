4690551e16b966288f126b7331f397b211a991a5
intira-bk <61378901+intira-bk@users.noreply.github.com>
2020-04-05 12:59:59 +0700

    Update feature4-1.ino

--- Assignment 4/4-1/feature4-1.ino
+++ Assignment 4/4-1/feature4-1.ino
@@ -1,6 +1,10 @@
 String yourAreYoung(int age) {
   String result;
-
+result = "I am"+ String(age)+"year old";
+if(age<18)
+{
+    result = "I am"+String(age)+"year old""your are yong";
+}
 
 
   return result;
