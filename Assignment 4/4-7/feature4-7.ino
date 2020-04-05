3525018ab5e054b5f7cf19246144bed94e51c32a
Peerapon Chaipat <kisi550d@gmail.com>
2020-04-05 19:09:33 +0700

    4-7 Clear

--- Assignment 4/4-7/feature4-7.ino
+++ Assignment 4/4-7/feature4-7.ino
@@ -1,6 +1,18 @@
 String multiplication(int multiple){
     String result;
-
-
+int i =multiple*1;int ii =multiple*2;int iii =multiple*3;int iv =multiple*4;int v =multiple*5;int vi =multiple*6;
+int vii =multiple*7;int viii =multiple*8;int ix =multiple*9;int x =multiple*10;int xi =multiple*11;int xii =multiple*12;
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(i)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(ii)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(iii)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(iv)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(v)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(vi)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(vii)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(viii)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(ix)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(x)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(xi)+"");
+Serial.println(""+String(multiple)+"\t*\t1\t=\t"+String(xii)+"");
     return result;
 }
