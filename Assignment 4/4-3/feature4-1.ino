16567a1afad1e7d227a1701970d79a57c5d2dc9c
salunchana Sonkhuntod <Salunchana2543@gmail.com>
2020-04-06 14:18:07 +0700

    feature4-1

--- Assignment 4/4-3/feature4-1.ino
+++ Assignment 4/4-3/feature4-1.ino
@@ -1,11 +1,14 @@
 String yourAreYoung(int age) {
   String result;
-
   return result;
 }
-
-String yourAreYoung(int age, String ageString) {
+  String yourAreYoung(int age, String ageString){ 
   String result;
 
+  result = "I'm"+ String(age)+"year old";
+  if(age <= 18 && age >0){
+  result = "I'm"+ String(age)+
+  "year old\nYour are young.";
+  }
   return result;
 }
