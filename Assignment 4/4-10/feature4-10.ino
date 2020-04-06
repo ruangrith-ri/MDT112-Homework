80c345fab968e0248fedc89c0ce2d1d3c6e0f5f6
Peerapon Chaipat <kisi550d@gmail.com>
2020-04-06 23:14:50 +0700

    clear

--- Assignment 4/4-10/feature4-10.ino
+++ Assignment 4/4-10/feature4-10.ino
@@ -1,14 +1,29 @@
 String primeNumber(int maxNumber) {
-  String result;
-    for (int i=2; i<=maxNumber; i++) {
-        
-            if((i%2)!=0||(i%3)!=0||(i%5)!=0||(i%7)!=0)
-            {
-            result = (""+String(i)+"\t");
-            i++;
+    int check = 0;
+    String result;
+
+    for (int i = 2; i <= maxNumber; i++) {
+        bool prime = true;
+        for (int j =2 ;j*j <= i ; j++) {
+            if(i % j == 0){
+                prime = false;
+                break;
             }
+
+        }
+        if(prime == true) {
+            result = result + String(i);
+
+            check = check + 1;
+
+            result += String("\t");
+        }
+
+        if(check == 5) {
+              result = result + "\n";
+
+              check = 0;
+          }
     }
-return result+result+result+result+result;}
-
-
-
+    return result;
+}
