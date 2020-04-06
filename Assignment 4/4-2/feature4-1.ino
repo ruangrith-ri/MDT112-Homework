1235dcdbb9f627fd7aacf915c891b8fb02c4cb82
gottyez <61343412+gottyez@users.noreply.github.com>
2020-04-06 17:56:19 +0700

    add fea 4-2(1)

--- Assignment 4/4-2/feature4-1.ino
+++ Assignment 4/4-2/feature4-1.ino
@@ -1,6 +1,10 @@
 String yourAreYoung(int age) {
   String result;
-
-
+  if (age >= 18) {
+      result = "I'am " + String(age) + " Year old ";
+  }
+  if (age < 18 && age > 0) {
+      result = "I'am " + String(age) + " year old \nyour are young.";
+  }
   return result;
 }
