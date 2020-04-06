050b52f31b9a9e478a9fc4f4e1cdafc91f19c07d
sawarin <sawarin.katoy@mail.kmutt.ac.th>
2020-04-06 23:03:43 +0700

    แก้ Homework ครั้งที่ 5

--- Assignment 4/4-8/feature4-8.ino
+++ Assignment 4/4-8/feature4-8.ino
@@ -1,5 +1,5 @@
 String multiplicationTable(int multiple) {
-    String result, sum, sum2, head;
+    String result, sum1, sum2, head;
     int calculation;
 
     for (int i = 2; i <= 12; i++) {
@@ -7,12 +7,12 @@ String multiplicationTable(int multiple) {
 
         if (i >= 12) {
             head = "-------------------------------------------------\n|\t\t\t" + String(multiple) + "\t\t\t|\n|-----------------------------------------------|\n";
-            sum = "|\t" + String(multiple) + "\t*\t" + String(i) + "\t=\t" + String(calculation) + "\t|" + "\n" +"-------------------------------------------------" + "\n";
+            sum1 = "|\t" + String(multiple) + "\t*\t" + String(i) + "\t=\t" + String(calculation) + "\t|" + "\n" +"-------------------------------------------------" + "\n";
         } 
         else {
-            sum = "|\t" + String(multiple) + "\t*\t" + String(i) + "\t=\t" + String(calculation) + "\t|" + "\n";
+            sum1 = "|\t" + String(multiple) + "\t*\t" + String(i) + "\t=\t" + String(calculation) + "\t|" + "\n";
         }
-    sum2 = sum2 + sum;
+    sum2 = sum2 + sum1;
     }
     result = head + sum2;
 
