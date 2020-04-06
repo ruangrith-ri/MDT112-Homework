224f9218c4c35b5a89ccca36df18921b993f762e
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-04-06 23:15:40 +0700

    9Success

--- Assignment 4/4-9/feature4-9.ino
+++ Assignment 4/4-9/feature4-9.ino
@@ -1,5 +1,17 @@
 String drawTriangle(int maxNumber) {
   String result;
+  for(int i = 1;i<=maxNumber;i++){
+      for(int j = 1;j<=i;j++){
+          result = result + ("*");
+      }
+      result = result + "\n";
+  }
+  for(int k = maxNumber-1;k>=1;k--){
+      for(int l = k;l>=1;l--){
+          result = result + ("*");
+      }
+      result = result +"\n";
+  }
 
   return result;
 }
