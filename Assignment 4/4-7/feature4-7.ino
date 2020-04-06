ec55ea7741d2df6d98dac7aa5e917e13ab5fb0df
salunchana Sonkhuntod <Salunchana2543@gmail.com>
2020-04-06 14:19:35 +0700

    feature4-7

--- Assignment 4/4-7/feature4-7.ino
+++ Assignment 4/4-7/feature4-7.ino
@@ -1,6 +1,21 @@
 String multiplication(int multiple){
-    String result;
+    String result ,sum,sum2;
+    int calculation;
+  for(int i=1 ;i <= 12 ;i++){
+    calculation = i*multiple;
+  if(i>=12){
+    sum = String(multiple)+"\r*\r" +String(i)+
+    "\r=\r"+String(calculation)+"\n";
+  }
+   else{
+    sum = String(multiple)+"\r*\r" +String(i)+
+    "\r=\r"+String(calculation)+"\n";
+  
+   }
+   sum2 = sum2+sum;
+  }
+  result = sum2;
+  
 
-
-    return result;
+   return result;
 }
