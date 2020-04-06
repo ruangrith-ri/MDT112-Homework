27afbdfbb463d3e803e3a0406d3d89526be784db
Tanapak Ngamsappakit <tanapakngamsuppaki@gmail.com>
2020-04-06 22:26:48 +0700

    no finish

---
+++ Assignment 4/4-10/feature4-10.ino
@@ -0,0 +1,24 @@
+String primeNumber(int maxNumber) 
+{
+ String result;
+{
+  
+  for(int i= 1;i<=maxNumber;i++)
+   {
+     
+    if(i%2 !=0 && i %3!=0 && i %5!=0 && i%7!=0)
+     {
+         
+        result =result+(" "+String (i)+ " "); 
+     
+         result =result+("\n");
+        
+     }
+    
+   }  
+    
+   
+  return result;
+  }
+
+}
