01d71d06d8671bf9c563718833eb81a2c095e713
intira-bk <61378901+intira-bk@users.noreply.github.com>
2020-04-06 12:55:43 +0700

    Update feature4-10.ino

--- Assignment 4/4-11/feature4-10.ino
+++ Assignment 4/4-11/feature4-10.ino
@@ -1,31 +1,5 @@
 String primeNumber(int maxNumber) {
-  String result,result1,result2,result3,result4,result5,result6,result7,result8,result9,result10,result11,result12,result13,result14,result15;;
-   int I =multiple*1;
-    int II=multiple*2;
-    int III=multiple*3;
-    int IV=multiple*4;
-    int V=multiple*5;
-    int VI=multiple*6;
-    int VII=multiple*7;
-    int VIII=multiple*8;
-    int IX=multiple*9;
-    int X=multiple*10;
-    int XI=multiple*11;
-    int XII=multiple*12;
-    result  =   ("______________________________________________");
-    result1 =   ("\n|                   "+String(mutiple)+"                  |\t");
-    result3 =   ("\n|\t"+String(multiple)+"\t*\t1\t=\t"+String(I)   +"\t|");
-    result4 =   ("\n|\t"+String(multiple)+"\t*\t2\t=\t"+String(II)  +"\t|");
-    result5 =   ("\n|\t"+String(multiple)+"\t*\t3\t=\t"+String(III) +"\t|");
-    result6 =   ("\n|\t"+String(multiple)+"\t*\t4\t=\t"+String(IV)  +"\t|");
-    result7 =   ("\n|\t"+String(multiple)+"\t*\t5\t=\t"+String(V)   +"\t|");
-    result8 =   ("\n|\t"+String(multiple)+"\t*\t6\t=\t"+String(VI)  +"\t|");
-    result9 =   ("\n|\t"+String(multiple)+"\t*\t7\t=\t"+String(VII) +"\t|");
-    result10=   ("\n|\t"+String(multiple)+"\t*\t8\t=\t"+String(VIII)+"\t|");
-    result11=   ("\n|\t"+String(multiple)+"\t*\t9\t=\t"+String(IX)  +"\t|");
-    result12=   ("\n|\t"+String(multiple)+"\t*\t10\t=\t"+String(X)  +"\t|");
-    result13=   ("\n|\t"+String(multiple)+"\t*\t11\t=\t"+String(XI) +"\t|");
-    result14=   ("\n|\t"+String(multiple)+"\t*\t12\t=\t"+String(XII)+"\t|");
-    result15=   ("\n______________________________________________");
-  return result,result1,result2,result3,result4,result5,result6,result7,result8,result9,result10,result11,result12,result13,result14,result15;;
+  String result;
+   
+  return result;
 }
