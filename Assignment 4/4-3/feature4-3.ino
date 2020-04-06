989c57b0da63ea2f64291f0d6fed8d10a4d32dfb
salunchana Sonkhuntod <Salunchana2543@gmail.com>
2020-04-06 14:16:13 +0700

    feature4-3

--- Assignment 4/4-3/feature4-3.ino
+++ Assignment 4/4-3/feature4-3.ino
@@ -1,5 +1,12 @@
 String numberToText(int age) {
   String result;
-
+  if(age==1){result = "one";
+  }
+  if(age==2){result = "two";
+  }
+  if(age==3){result = "three";
+  }
+  if(age==4){result = "four";
+  }
   return result;
 }
