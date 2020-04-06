c2f1f9e393e9f5d788e37f7ee3f98780f72a1ae2
salunchana Sonkhuntod <Salunchana2543@gmail.com>
2020-04-06 16:20:19 +0700

    feature4-1

--- Assignment 4/4-3/feature4-1.ino
+++ Assignment 4/4-3/feature4-1.ino
@@ -1,14 +1,16 @@
+String numberToText(int age);// if without this line code run normally
 String yourAreYoung(int age) {
   String result;
+
   return result;
 }
-  String yourAreYoung(int age, String ageString){ 
-  String result;
 
-  result = "I'm"+ String(age)+"year old";
-  if(age <= 18 && age >0){
-  result = "I'm"+ String(age)+
-  "year old\nYour are young.";
+String yourAreYoung(int age, String ageString) {
+  String result;
+  result = "I'm " + numberToText(age) + " year old"; //if without line1 code numberToText(age) have "Use of indeclared identifier" ????
+  if(age<=18 && age >0){
+      result = "I'm " + numberToText(age) + " year old\nYour are young."; //if without line1 code numberToText(age) have "Use of indeclared identifier"???
   }
+
   return result;
 }
