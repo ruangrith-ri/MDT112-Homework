dcb7750350a92040837b7203d4a78e45a903b409
Noppamas045 <noppamas.sang@mail.kmutt.ac.th>
2020-04-06 22:40:26 +0700

    edit4 ;;;;;; ยากกกกก

--- Assignment 4/4-10/feature4-10.ino
+++ Assignment 4/4-10/feature4-10.ino
@@ -1,25 +1,30 @@
 String primeNumber(int maxNumber) {
-  String result;
-  for (int i = 2; i <= maxNumber; i++) {
-      bool prim = true;
-      for (int j =2 ;j*j <= i ; j++) {
-          if(i % j == 0){
-              prim = false;
-              break;
+    int check = 0;
+    char key_space = ' ';
+    String result;
+
+    for (int i = 2; i <= maxNumber; i++) {
+        bool prime = true;
+        for (int j =2 ;j*j <= i ; j++) {
+            if(i % j == 0){
+                prime = false;
+                break;
+            }
+
+        }
+        if(prime == true) {
+            result = result + String(i);
+
+            check = check + 1;
+
+            result += String("\t");
+        }
+
+        if(check == 5) {
+              result = result + "\n";
+
+              check = 0;
           }
-          
-      }
-      if(prim)
-      
-      result = result + String(i); 
-      
-
-  }
-
-  
-  
-
-  
-
-  return result;
+    }
+    return result;
 }
