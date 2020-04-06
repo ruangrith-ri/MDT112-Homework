4d6dfa50b2f302405fd8dfd3b2cc6d3e777221bb
salunchana Sonkhuntod <Salunchana2543@gmail.com>
2020-04-06 15:40:27 +0700

    feature4-3

--- Assignment 4/4-3/feature4-3.ino
+++ Assignment 4/4-3/feature4-3.ino
@@ -8,5 +8,37 @@ String numberToText(int age) {
   }
   if(age==4){result = "four";
   }
+   if(age == 5){result = "five";
+   }
+   if(age == 6){result = "six";
+   }
+   if(age == 7){result = "seven";
+   }
+   if(age == 8){result = "eight";
+   }
+   if(age == 9){result = "nine";
+   }
+   if(age == 10){result = "ten";
+   }
+   if(age == 11){result = "eleven";
+   }
+   if(age == 12){result = "twelve";
+   }
+   if(age == 13){result = "thirteen";
+   }
+   if(age == 14){result = "fourteen";
+   }
+   if(age == 15){result = "fifteen";
+   }
+   if(age == 16){result = "sixteen";
+   }
+   if(age == 17){result = "seventeen";
+   }
+   if(age == 18){result = "eighteen";
+   }
+   if(age == 19){result = "nineteen";
+   }
+   if(age == 20){result = "twenty";
+   }
   return result;
 }
