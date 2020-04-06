1e4a1554b2e2fdea4dcd7cd40a5ecc94afbd58b0
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-04-07 00:00:47 +0700

    8Success

--- Assignment 4/4-8/feature4-8.ino
+++ Assignment 4/4-8/feature4-8.ino
@@ -1,5 +1,16 @@
 String multiplicationTable(int multiple) {
-  String result;
-
+  String result, stack2, stack1, table;
+  int calculation;
+  for (int i = 2; i <= 12; i++) {
+        calculation = i * multiple;
+        table = "-------------------------------------------------\n|\t\t\t" + String(multiple) + "\t\t\t|\n|-----------------------------------------------|\n";
+        stack1 = "|\t" +String(multiple) + "\t * \t" +String(i)+ "\t  =  \t" + String(calculation) + "\t|"+ "\n"  ;
+        
+        
+        stack2 = stack2+stack1;
+      
+    }
+  result = table + stack2;
   return result;
+
 }
