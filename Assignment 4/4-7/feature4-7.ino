8e398cdc8547bc89ed5f7d0a46da43c23418dec4
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-04-06 23:09:57 +0700

    6-7Success

--- Assignment 4/4-7/feature4-7.ino
+++ Assignment 4/4-7/feature4-7.ino
@@ -1,5 +1,17 @@
 String multiplication(int multiple){
-    String result;
+    String result,stack2,stack1;
+    int calculation;
+    for(int i=1;i<=12;i++){
+        calculation = i*multiple;
+        
+        stack1 = String(multiple) + "\t * \t" +String(i)+ "\t  =  \t" + String(calculation) + "\n";
+        
+        
+        stack2 = stack2+stack1;
+        
+    }
+    result = stack2;
+    
 
 
     return result;
