8aa6f42d5e91d9d99b2aec3dd0252026f645b29e
salunchana Sonkhuntod <Salunchana2543@gmail.com>
2020-04-06 14:18:28 +0700

    feature4-1

--- Assignment 4/4-1/feature4-1.ino
+++ Assignment 4/4-1/feature4-1.ino
@@ -1,7 +1,10 @@
-String yourAreYoung(int age) {
+  String yourAreYoung(int age){ 
   String result;
-
-
-
-  return result;
+    result = "I'm" + String(age)+" year old ";
+ 
+   if (age <= 18 && age >0) {
+        result= "I'm" + String(age)+" year old\n"
+        "Your are young."; 
+   }
+   return result; 
 }
