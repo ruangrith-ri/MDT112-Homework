340dabeef428920af21fc76a3a6195ce70a37937
orawan onsang <orawan.onsang@mail.kmutt.ac.th>
2020-04-06 07:23:35 -0800

    commit

--- Assignment 4/4-1/feature4-1.ino
+++ Assignment 4/4-1/feature4-1.ino
@@ -1,7 +1,13 @@
-String yourAreYoung(int age) {
-  String result;
-
-
-
+String yourAreYoung(int age)
+{
+  String result; 
+  if (age >= 18) 
+  { 
+      result = "I'm " + String(age) + " years old";
+  }
+  if(age < 18 && age > 0)
+  {
+      result = "I'm " + String(age) + " years old\n You are  young.";    
+  }
   return result;
-}
+} 
