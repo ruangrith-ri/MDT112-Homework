3fba793c73544cc879a49a8f457770e97dd26404
pattamawan meelakchai <pattamawan.p@mail.kmutt.ac.th>
2020-05-17 16:51:53 +0700

    Module6

--- Assignment 4/4-8/feature4-8.ino
+++ Assignment 4/4-8/feature4-8.ino
@@ -1,5 +1,26 @@
 String multiplicationTable(int multiple) {
-  String result;
+  String result, sum, sum2, head;
+  int calculation;
+
+  for (int i = 2; i <= 12; i++) {
+    calculation = i * multiple;
+
+    if (i >= 12) {
+      head = "-------------------------------------------------\n|\t\t\t" +
+             String(multiple) +
+             "\t\t\t|\n|-----------------------------------------------|\n";
+      sum = "|\t" + String(multiple) + "\t*\t" + String(i) + "\t=\t" +
+            String(calculation) + "\t|" + "\n" +
+            "-------------------------------------------------" + "\n";
+    } else {
+      sum = "|\t" + String(multiple) + "\t*\t" + String(i) + "\t=\t" +
+            String(calculation) + "\t|" + "\n";
+    }
+
+    sum2 = sum2 + sum;
+  }
+  result = head + sum2;
+
 
   return result;
 }
