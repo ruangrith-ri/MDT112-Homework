892d0115638512d1dfe0127a40fa670abb457312
Noppamas045 <noppamas.sang@mail.kmutt.ac.th>
2020-05-23 02:49:29 +0700

    HW Module 6

--- Assignment 3/3-1/3-1.ino
+++ Assignment 3/3-1/3-1.ino
@@ -1,43 +1,31 @@
+long lastTimeStateChange = 0;
+String state = "ON" ;
 void setup(){
-    pinMode(2,OUTPUT);
-    pinMode(3,OUTPUT);
-    pinMode(4,OUTPUT);
-    pinMode(5,OUTPUT);
-    pinMode(6,OUTPUT);
-    pinMode(7,OUTPUT);
-    pinMode(8,OUTPUT);
-    pinMode(9,OUTPUT);
-    pinMode(10,OUTPUT);
-    pinMode(11,OUTPUT);
-    pinMode(12,OUTPUT);
-    pinMode(13,OUTPUT);    
-}
-void loop(){
-    digitalWrite(2,1);
-    digitalWrite(3,1);
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-    digitalWrite(6,1);
-    digitalWrite(7,1);
-    digitalWrite(8,1);
-    digitalWrite(9,1);
-    digitalWrite(10,1);
-    digitalWrite(11,1);
-    digitalWrite(12,1);
-    digitalWrite(13,1);
-    delay(500);
+    for (int i = 2; i <=13; i++) {
+        pinMode(i,OUTPUT);
+    
+    }
 
-    digitalWrite(2,0);
-    digitalWrite(3,0);
-    digitalWrite(4,0);
-    digitalWrite(5,0);
-    digitalWrite(6,0);
-    digitalWrite(7,0);
-    digitalWrite(8,0);
-    digitalWrite(9,0);
-    digitalWrite(10,0);
-    digitalWrite(11,0);
-    digitalWrite(12,0);
-    digitalWrite(13,0);
-    delay(500);
+}
+
+void loop(){
+    long currentTime = millis();
+
+    if (state == "ON" && currentTime - lastTimeStateChange >= 500) {
+       state = "OFF";
+       lastTimeStateChange = currentTime ;
+    }
+    else if (state == "OFF" && currentTime - lastTimeStateChange >= 500 ){
+       state = "ON" ;
+       lastTimeStateChange = currentTime;
+    }
+    
+    for (int i = 2; i <= 13 ;i++) {
+       if(state == "ON"){
+        digitalWrite(i,1);
+
+       }else if (state == "OFF"){
+        digitalWrite(i,0);
+       }   
+    }
 }
--- Assignment 3/3-2/3-2.ino
+++ Assignment 3/3-2/3-2.ino
@@ -1,46 +1,32 @@
-void setup(){
-    pinMode(2,OUTPUT);
-    pinMode(3,OUTPUT);
-    pinMode(4,OUTPUT);
-    pinMode(5,OUTPUT);
-    pinMode(6,OUTPUT);
-    pinMode(7,OUTPUT);
+long lastTimeStateChange = 0;
+String state = "ON";
 
-    pinMode(8,OUTPUT);
-    pinMode(9,OUTPUT);
-    pinMode(10,OUTPUT);
-    pinMode(11,OUTPUT);
-    pinMode(12,OUTPUT);
-    pinMode(13,OUTPUT); 
+void setup(){
+    for (int pin = 2; pin <= 13; pin++) {
+    pinMode(pin,OUTPUT);
+    }
 }
 
 void loop(){
-    digitalWrite(2,1);
-    digitalWrite(3,1);
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-    digitalWrite(6,1);
-    digitalWrite(7,1);
-    digitalWrite(8,0);
-    digitalWrite(9,0);
-    digitalWrite(10,0);
-    digitalWrite(11,0);
-    digitalWrite(12,0);
-    digitalWrite(13,0);
-    delay(500);
-    digitalWrite(8,1);
-    digitalWrite(9,1);
-    digitalWrite(10,1);
-    digitalWrite(11,1);
-    digitalWrite(12,1);
-    digitalWrite(13,1);
-    digitalWrite(2,0);
-    digitalWrite(3,0);
-    digitalWrite(4,0);
-    digitalWrite(5,0);
-    digitalWrite(6,0);
-    digitalWrite(7,0);
-    delay(500);
+    long currentTime = millis();
 
+    if(state == "ON" && currentTime - lastTimeStateChange >= 500){
+        state = "OFF";
+        lastTimeStateChange = currentTime;
+    }else if (state == "OFF" && currentTime - lastTimeStateChange >= 500 ){
+        state = "ON";
+        lastTimeStateChange = currentTime;
+    }
 
+    for(int i = 2 ; i <= 13 ; i++){
+        if (state == "ON" && i <= 7) {
+        digitalWrite(i,1);
+        }else if (state == "OFF" && i <= 7) {
+        digitalWrite(i,0);
+        }else if (state == "OFF" && i > 7) {
+        digitalWrite(i,1);
+        }else if (state == "ON" && i > 7) {
+        digitalWrite(i,0);
+        }
+   }
 }
