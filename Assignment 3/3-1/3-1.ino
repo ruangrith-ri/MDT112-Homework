4bbea3ce9c5242cef7bce23847a7d1437120533d
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-05-25 00:52:50 +0700

    homework

--- Assignment 3/3-1/3-1.ino
+++ Assignment 3/3-1/3-1.ino
@@ -1,49 +1,35 @@
+long timechange =  0;
+String state = "ON";
 void setup(){
+    Serial.begin(9600);
+    for(int x = 2 ; x<= 13 ; x++){
+        pinMode(x, OUTPUT);
+    }
     
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
-    pinMode(8, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
-    pinMode(14, OUTPUT);
 }
-void loop(){
-    digitalWrite(2, 1);
-    digitalWrite(3, 1);
-    digitalWrite(4, 1);
-    digitalWrite(5, 1);
-    digitalWrite(6, 1);
-    digitalWrite(7, 1);
-    digitalWrite(8, 1);
-    digitalWrite(9, 1);
-    digitalWrite(10, 1);
-    digitalWrite(11, 1);
-    digitalWrite(12, 1);
-    digitalWrite(13, 1);
-    delay(500);
-
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0);
-    digitalWrite(11, 0);
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
-    delay(500);
-
-
-
-    
-}   
+void loop()
+{
+    long simulation  = millis();        
+    if (state == "ON")
+    {
+        for (int y = 2 ; y <= 13 ; y++)
+        {digitalWrite(y, 1);
+        }
+            if (simulation - timechange >= 500)      
+            {
+            state = "OFF";      
+            timechange = simulation;
+            }
+    }
+    if (state == "OFF")
+    {
+        for (int y = 2 ; y <= 13 ; y++)
+        {digitalWrite(y, 0);
+        }
+            if (simulation - timechange >= 500)       
+            {
+            state = "ON";       
+            timechange = simulation;
+            }
+    }
+}
--- Assignment 3/3-2/3-2.ino
+++ Assignment 3/3-2/3-2.ino
@@ -1,45 +1,55 @@
+long timechange =  0;
+String state = "ON";
 void setup(){
-    pinMode(2, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
-    pinMode(8, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
-    pinMode(14, OUTPUT);
+    Serial.begin(9600);
+    for(int x = 2 ; x<= 13 ; x++){
+        pinMode(x, OUTPUT);
+    }
+    
 }
 
 void loop(){
+    long simulationdalay = millis();
+    if (state == "ON")
+    {
     digitalWrite(2, 1);
     digitalWrite(3, 1);
     digitalWrite(4, 1);
     digitalWrite(5, 1);
     digitalWrite(6, 1);
     digitalWrite(7, 1);
-    delay(500);
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
-    delay(75);
-    digitalWrite(8, 1);
-    digitalWrite(9, 1);
-    digitalWrite(10, 1);
-    digitalWrite(11, 1);
-    digitalWrite(12, 1);
-    digitalWrite(13, 1);
-    delay(500);
     digitalWrite(8, 0);
     digitalWrite(9, 0);
     digitalWrite(10, 0);
     digitalWrite(11, 0);
     digitalWrite(12, 0);
     digitalWrite(13, 0);
-    delay(75);
+            if (simulationdalay - timechange >= 500)      
+            {
+            state = "OFF";      
+            timechange = simulationdalay;
+            }
+    }
+    if (state == "OFF")
+    {
+    digitalWrite(2, 0);
+    digitalWrite(3, 0);
+    digitalWrite(4, 0);
+    digitalWrite(5, 0);
+    digitalWrite(6, 0);
+    digitalWrite(7, 0);
+    digitalWrite(8, 1);
+    digitalWrite(9, 1);
+    digitalWrite(10, 1);
+    digitalWrite(11, 1);
+    digitalWrite(12, 1);
+    digitalWrite(13, 1);
+            if (simulationdalay - timechange >= 500)      
+            {
+            state = "ON";      
+            timechange = simulationdalay;
+            }
+    }
+
+    
 }
