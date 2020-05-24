639b93a6bf1f9b0ffc200b56dd9996efe4a10658
Peerapon Chaipat <kisi550d@gmail.com>
2020-05-24 19:07:52 +0700

    Change Delay

--- Assignment 3/3-1/3-1.ino
+++ Assignment 3/3-1/3-1.ino
@@ -1,3 +1,5 @@
+long lasttime = 0;
+String state = "ON";
 void setup(){
     //Serial.begin(9600);
 pinMode(2, OUTPUT);
@@ -16,6 +18,11 @@ pinMode(13, OUTPUT);
 }
 
 void loop(){
+long current = millis();
+    if(state == "ON" && current - lasttime >= 500){
+        state = "OFF";
+        lasttime = current;
+        if(state == "ON"){
 digitalWrite(2,1);
 digitalWrite(3,1);
 digitalWrite(4,1);
@@ -28,8 +35,9 @@ digitalWrite(10,1);
 digitalWrite(11,1);
 digitalWrite(12,1);
 digitalWrite(13,1);
-delay(500);
-digitalWrite(2,0);
+
+        }else if (state == "OFF"){
+        digitalWrite(2,0);
 digitalWrite(3,0);
 digitalWrite(4,0);
 digitalWrite(5,0);
@@ -41,5 +49,36 @@ digitalWrite(10,0);
 digitalWrite(11,0);
 digitalWrite(12,0);
 digitalWrite(13,0);
-delay(500);
+    }
+    }else if (state == "OFF" && current - lasttime >= 500){
+        state = "ON";
+        lasttime = current;
+        if(state == "ON"){
+        digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,1);
+digitalWrite(5,1);
+digitalWrite(6,1);
+digitalWrite(7,1);
+digitalWrite(8,1);
+digitalWrite(9,1);
+digitalWrite(10,1);
+digitalWrite(11,1);
+digitalWrite(12,1);
+digitalWrite(13,1);
+        }else if (state == "OFF"){
+        digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,0);
+digitalWrite(5,0);
+digitalWrite(6,0);
+digitalWrite(7,0);
+digitalWrite(8,0);
+digitalWrite(9,0);
+digitalWrite(10,0);
+digitalWrite(11,0);
+digitalWrite(12,0);
+digitalWrite(13,0);
+        }
+    }
 }
