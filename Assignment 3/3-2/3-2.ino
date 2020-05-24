17249c7734f366641b717c03edafab8483abb7f6
orawan onsang <orawan.onsang@mail.kmutt.ac.th>
2020-05-24 22:44:02 +0700

    interrupe

--- Assignment 3/3-2/3-2.ino
+++ Assignment 3/3-2/3-2.ino
@@ -1,3 +1,4 @@
+/* 
 void setup() {
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
@@ -12,7 +13,9 @@ void setup() {
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
 }
+*/
 
+/* 
 void loop() {
   digitalWrite(2, 1);
   digitalWrite(3, HIGH);
@@ -45,4 +48,73 @@ void loop() {
   digitalWrite(12, 1);
   digitalWrite(13, HIGH);
   delay(500);
+}
+*/
+
+//-------------------------------
+
+long lastTimeStataChange = 0; 
+String state = "ON";
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+    long currentTime = millis(); //คำสั่งที่ใช้งาน Delay
+    
+    if (state == "ON" && currentTime - lastTimeStataChange >= 500 ){ //เวลาปัจจุบัน - การเปลี่ยนสถานะเวลาครั้งล่าสุด
+        state = "OFF";
+        lastTimeStataChange = currentTime; 
+    }
+    else if (state == "OFF" && currentTime - lastTimeStataChange >= 500 ) {
+        state = "ON";
+        lastTimeStataChange = currentTime; 
+    }
+
+    if (state == "ON"){
+        digitalWrite(2, 1);
+        digitalWrite(3, 1);
+        digitalWrite(4, 1);
+        digitalWrite(5, 1);
+        digitalWrite(6, 1);
+        digitalWrite(7, 1);
+        delay(500);
+    }
+
+    if (state == "OFF"){
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+        delay(500);
+
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        delay(500);
+
+        digitalWrite(8, 1);
+        digitalWrite(9, 1);
+        digitalWrite(10, 1);
+        digitalWrite(11, 1);
+        digitalWrite(12, 1);
+        digitalWrite(13, 1);
+        delay(500);
+    }
 }
